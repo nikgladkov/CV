852ea1a6e6414c153e3831a5676659f9faddba3a
Nik <106027727+nikgladkov@users.noreply.github.com>
2024-11-28 15:50:53 +0100

    Update github-report.js

--- cypress_project/github-report.js
+++ cypress_project/github-report.js
@@ -1,8 +1,8 @@
 const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
-  jsonDir: "reports_github",
-  reportPath: "reports_github",
+  jsonDir: "/reports_github",
+  reportPath: "/reports_github",
   metadata: {
     device: "GitHub",
   },
